882a201d0b98183b6abb1b236230e3c972a8a0e9
sarthak <sarthaksri044@gmail.com>
2023-06-29 01:38:42 +0530

    code

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,8 +13,9 @@ exports.auth = (req, res, next) => {
                 message: "token missing"
             })
         }
-         // verify the token 
-         try {
+
+        // verify the token 
+        try {
             const decode = jwt.verify(token, process.env.JWT_SECRET);
 
             console.log(decode)
@@ -35,6 +36,42 @@ exports.auth = (req, res, next) => {
         return res.status(401).json({
             success: false,
             message: "Something went wrong while verifying token"
+        })
+    }
+}
+
+exports.isStudent = (req, res, next) => {
+    try {
+        if (req.user.role !== "Student") {
+            return res.status(401).json({
+                success: false,
+                message: "This is a protect route for students you can not access it"
+            })
+        }
+        next();
+    }
+    catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "User Role is not Matching"
+        })
+    }
+}
+
+exports.isAdmin = (req, res, next) => {
+    try {
+        if (req.user.role !== "Admin") {
+            return res.status(401).json({
+                success: false,
+                message: "This is a protect route for Admins,you can not access it"
+            })
+        }
+        next();
+    }
+    catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: "User Role is not Matching"
         })
     }
 }

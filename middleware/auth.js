9b1bee89d296b6b0207f2d08b0220a5b1ab496bf
sarthak <sarthaksri044@gmail.com>
2023-06-30 00:21:11 +0530

    changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,8 +4,13 @@ require("dotenv").config();
 
 exports.auth = (req, res, next) => {
     try {
-        const token = req.body.token;
+        // const token = req.body.token;
         // const token = req.cookie.token 
+        console.log("cookie" , req.cookies.token);
+        console.log("body" , req.body.token);
+        console.log("header", req.header("Authorization"));
+       
+        const token = req.cookies.token || req.body.token || req.header("Authorization").replace("Bearer ", "");
 
         if (!token) {
             return res.status(401).json({
